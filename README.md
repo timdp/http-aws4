d88e015e86c15e7cbe71342bfc10dfabfd96e64e
Tim De Pauw <timdp@users.noreply.github.com>
2017-01-15 14:30:57 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# http-aws4
+
+[![npm](https://img.shields.io/npm/v/http-aws4.svg)](https://www.npmjs.com/package/http-aws4) [![Dependencies](https://img.shields.io/david/timdp/http-aws4.svg)](https://david-dm.org/timdp/http-aws4) [![JavaScript Standard Style](https://img.shields.io/badge/code%20style-standard-brightgreen.svg)](http://standardjs.com/)
+
+Performs AWS Signature Version 4-signed HTTP requests from the command line.
+
+## Installation
+
+```bash
+npm i -g http-aws4
+```
+
+## Usage
+
+Perform a GET on an AWS Elasticsearch index:
+
+```bash
+haws https://ES-DOMAIN.us-east-1.es.amazonaws.com/INDEX-NAME
+```
+
+Delete the index:
+
+```bash
+haws delete https://ES-DOMAIN.us-east-1.es.amazonaws.com/INDEX-NAME
+```
+
+## Author
+
+[Tim De Pauw](https://github.com/timdp)
+
+## License
+
+MIT
---
+++ http-aws4.js
@@ -0,0 +1,142 @@
+#!/usr/bin/env node
+
+const AWS = require('aws-sdk')
+const cardinal = require('cardinal')
+const chalk = require('chalk')
+const cleanStack = require('clean-stack')
+const getStdin = require('get-stdin')
+const pify = require('pify')
+const yargs = require('yargs')
+
+const CONSOLE_COLORS = [
+  ['log', 'blue'],
+  ['info', 'green'],
+  ['warn', 'yellow'],
+  ['error', 'red']
+]
+
+const {_: args, region} = yargs
+  .demand(1)
+  .usage('Usage: $0 [options] [method] <url>')
+  .string('region').alias('r', 'region').default('region', 'us-east-1')
+  .string('service')
+  .argv
+let service, method, url
+if (/^\w+$/.test(args[0])) {
+  method = args.shift().toUpperCase()
+  url = args.shift()
+} else {
+  url = args.shift()
+  method = 'GET'
+}
+if (service == null) {
+  service = /(\w+)\.amazonaws\.com\//i.exec(url)[1]
+}
+const headers = {}
+while (args.length > 0) {
+  const arg = args.shift()
+  const pos = arg.indexOf(':')
+  headers[arg.substr(0, pos)] = arg.substr(pos + 1)
+}
+
+const config = new AWS.Config({region})
+const client = new AWS.NodeHttpClient()
+
+const logger = CONSOLE_COLORS.reduce((logger, [fn, color]) => {
+  const fmt = chalk[color]
+  logger[fn] = (...args) => {
+    console[fn].apply(console, args.map((arg) => fmt(arg)))
+  }
+  logger[fn].bare = (...args) => {
+    console[fn].apply(console, args)
+  }
+  return logger
+}, {})
+
+const formatHeaders = (headers, type) => {
+  for (const name of Object.keys(headers).sort()) {
+    logger[type](`${name}: ${headers[name]}`)
+  }
+}
+
+const formatResponse = (resp, type = 'info') => {
+  logger[type](resp.statusCode + ' ' + resp.statusMessage)
+  if (resp.headers) {
+    formatHeaders(resp.headers, type)
+  }
+  logger[type]()
+  let body = resp.body
+  if (/\bjson\b/.test(resp.headers['content-type'])) {
+    try {
+      body = JSON.stringify(JSON.parse(body), null, 2)
+      if (type !== 'error' && chalk.enabled) {
+        logger[type].bare(cardinal.highlight(body))
+        return
+      }
+    } catch (err) {}
+  }
+  logger[type]((body != null) ? body : '[empty response]')
+}
+
+const handleError = (err) => {
+  if (err.response != null) {
+    formatResponse(err.response, 'error')
+  } else {
+    logger.error(cleanStack(err.stack))
+  }
+  process.exit(1)
+}
+
+const createRequest = (method, url, body, credentials) => {
+  const endpoint = new AWS.Endpoint(url)
+  const request = Object.assign(new AWS.HttpRequest(endpoint), {
+    region,
+    method,
+    body
+  })
+  Object.assign(request.headers, headers, {Host: endpoint.host})
+  const signer = new AWS.Signers.V4(request, service)
+  signer.addAuthorization(credentials, new Date())
+  return request
+}
+
+const handleRequest = (request) => new Promise((resolve, reject) => {
+  logger.log(request.method + ' ' + request.endpoint.href)
+  formatHeaders(request.headers, 'log')
+  logger.log()
+  client.handleRequest(request, null, resolve, reject)
+})
+
+const handleResponse = (response) => new Promise((resolve, reject) => {
+  response.setEncoding('utf8')
+  const metadata = {
+    statusCode: response.statusCode,
+    statusMessage: response.statusMessage,
+    headers: response.headers,
+    body: ''
+  }
+  response.on('data', (data) => {
+    metadata.body += data
+  })
+  response.on('end', () => {
+    if (metadata.statusCode < 200 || metadata.statusCode >= 300) {
+      const error = new Error()
+      error.response = metadata
+      reject(error)
+    } else {
+      resolve(metadata)
+    }
+  })
+  response.on('error', reject)
+})
+
+const getCredentials = pify(config.getCredentials).bind(config)
+
+Promise.all([getStdin(), getCredentials()])
+  .then(([body, credentials]) => {
+    const request = createRequest(method, url, body, credentials)
+    return handleRequest(request)
+  })
+  .then(handleResponse)
+  .then(formatResponse)
+  .catch(handleError)
